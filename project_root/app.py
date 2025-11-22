0f46fb4301efce0e490963857a37d964568b0603
ILYASS BELMEHDI <99798385+Zzzoorroo@users.noreply.github.com>
2025-11-22 10:24:58 +0800

    ndjnfjd

--- project_root/app.py
+++ project_root/app.py
@@ -1,24 +1,23 @@
 from flask import Flask
+from models.database import db
 from routes.auth_routes import auth_bp
 from routes.dashboard_routes import dashboard_bp
 from routes.api_routes import api_bp
-
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
-    #Initialize the database
+    # Initialize the database
     db.init_app(app)
 
-    #Register blueprints
+    # Register blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(api_bp)
 
     return app
 
-
 if __name__ == "__main__":
-    app= create_app()
+    app = create_app()
     app.run(debug=True)
